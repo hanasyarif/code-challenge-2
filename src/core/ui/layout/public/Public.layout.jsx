622a8153dd33d06348ecd6a8eee8a874113aa871
hanasyarif <hanasyarif@gmail.com>
2023-12-10 13:04:01 +0700

    fix: link footer

--- src/core/ui/layout/public/Public.layout.jsx
+++ src/core/ui/layout/public/Public.layout.jsx
@@ -7,7 +7,6 @@ import Footer from "../../components/footer/Footer.component";
 import { getCompanyDictionaries } from "@/core/dictionaries/company";
 import { getContactDictionaries } from "@/core/dictionaries/contact";
 import { getMenuDictionaries } from "@/core/dictionaries/menu";
-import { useNavigationStore } from "@/core/zustand/navigation/store";
 import { useRouter, usePathname } from "next/navigation";
 
 export default function PublicLayout({ children }) {
@@ -15,35 +14,17 @@ export default function PublicLayout({ children }) {
   const menu = getMenuDictionaries(locale).alvisual;
   const company = getCompanyDictionaries(locale).alvisual;
   const contact = getContactDictionaries(locale).alvisual;
-  const navigationStore = useNavigationStore();
   const router = useRouter();
   const pathname = usePathname();
 
   const defaultLink = "/";
 
   const handleClickNavbar = (value) => {
-    if (value.includes("/")) {
-      router.push(value);
-      return;
-    }
-    navigationStore.setIDNavigation(value);
-    localStorage.setItem("navigation_id", value);
-    router.push(defaultLink);
+    router.push(value);
   };
   const handleClickFooter = (value) => {
-    navigationStore.setIDNavigation(value);
-    localStorage.setItem("navigation_id", value);
-    router.push(defaultLink);
+    router.push(value);
   };
-
-  useEffect(() => {
-    if (typeof window !== undefined) {
-      const navigationID = localStorage.getItem("navigation_id");
-      if (navigationID !== null) {
-        navigationStore.setIDNavigation(navigationID);
-      }
-    }
-  }, []);
 
   return (
     <main className={clsx("w-full")}>
