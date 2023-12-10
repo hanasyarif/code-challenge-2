84b0b4c9af3202f9d0e00bb9c6f1e1a34bb8e2c7
hanasyarif <hanasyarif@gmail.com>
2023-12-10 10:39:34 +0700

    fix: content services

--- src/features/home/constants/dictionaries/dictionaries.js
+++ src/features/home/constants/dictionaries/dictionaries.js
@@ -1,9 +1,7 @@
 import "server-only";
-import zh from "./locale/zh.json";
 import en from "./locale/en.json";
 
 const dictionaries = {
-  zh: zh,
   en: en,
 };
 export const getHomeFeatureDictionaries = (locale) => dictionaries[locale];
